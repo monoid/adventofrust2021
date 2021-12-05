a7c8007e65484c8511163b11a13578140731cad9
Ivan Boldyrev <lispnik@gmail.com>
2021-12-05 18:55:33 +0700

    day05

---
+++ day05/task2/src/main.rs
@@ -0,0 +1,66 @@
+use std::{
+    collections::HashMap,
+    io::{self, BufRead as _},
+    str::FromStr,
+};
+
+#[derive(Debug)]
+struct Line {
+    p0: (i32, i32),
+    p1: (i32, i32),
+}
+
+impl FromStr for Line {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        fn split_pair(p: &str) -> (i32, i32) {
+            let mut by_comma = p.split(',');
+            let x = by_comma.next().unwrap().parse().unwrap();
+            let y = by_comma.next().unwrap().parse().unwrap();
+            (x, y)
+        }
+        let mut by_space = s.split(' ');
+        let pair0 = by_space.next().unwrap();
+        by_space.next().unwrap(); // Arrow
+        let pair1 = by_space.next().unwrap();
+
+        Ok(Self {
+            p0: split_pair(pair0),
+            p1: split_pair(pair1),
+        })
+    }
+}
+
+impl Line {
+    fn get_points(&self) -> impl Iterator<Item = (i32, i32)> + 'static {
+        let p0: (_, _) = self.p0;
+        let p1 = self.p1;
+
+        let ax = (p1.0 - p0.0).abs();
+        let ay = (p1.1 - p0.1).abs();
+
+        let dx = (p1.0 - p0.0).signum();
+        let dy = (p1.1 - p0.1).signum();
+
+        let len = std::cmp::max(ax, ay);
+
+        (0..=len).map(move |i| (p0.0 + i * dx, p0.1 + i * dy))
+    }
+}
+
+fn main() {
+    let mut points = HashMap::<_, u32>::new();
+    for line_str in io::stdin().lock().lines() {
+        let line = Line::from_str(&line_str.unwrap()).unwrap();
+        for p in line.get_points() {
+            *points.entry(p).or_default() += 1;
+        }
+    }
+
+    let res = points
+        .into_values()
+        .filter_map(|v| if v > 1 { Some(()) } else { None })
+        .count();
+    println!("{}", res);
+}
