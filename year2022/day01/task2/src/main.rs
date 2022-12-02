ef493754e765f66ceed512d0da454ec4823062f7
Ivan Boldyrev <lispnik@gmail.com>
2022-12-02 09:24:01 +0100

    year 2022, day01: somewhat simplier

--- year2022/day01/task2/src/main.rs
+++ year2022/day01/task2/src/main.rs
@@ -1,17 +1,13 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
-
 const TOP: usize = 3;
 
 fn main() {
     let lines = lib::lines().map(Result::unwrap);
-    let elves = lib::elves(lines).into_iter();
-    let mut heap = BinaryHeap::default();
-    for elf in elves {
-        let w = elf.calories();
-        heap.push(Reverse(w));
-        if heap.len() > TOP {
-            heap.pop();
-        }
-    }
-    println!("{}", heap.into_iter().map(|v| v.0).sum::<u32>());
+    let mut elves: Vec<u32> = lib::elves(lines).into_iter().map(|elf| elf.calories()).collect();
+    let top = if elves.len() > TOP {
+        let len = elves.len();
+        elves.select_nth_unstable(len - TOP - 1).2
+    } else {
+        &elves[..]
+    };
+    println!("{}", top.iter().sum::<u32>());
 }
